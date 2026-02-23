16778b918f0923aa0d92af47c18213c6a46dd2f4
CorneliusRior <corneliuscriordan@gmail.com>
2026-02-23 23:02:10 +0000

    Commit before we do some experimentation with datagrid formatting.

--- MacroTrack.Application/ViewModels/GoalSetVM.cs
+++ MacroTrack.Application/ViewModels/GoalSetVM.cs
@@ -1,8 +1,10 @@
 ﻿using MacroTrack.AppLibrary.Commands;
 using MacroTrack.AppLibrary.Services;
+using MacroTrack.Core.Models;
 using MacroTrack.Core.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,20 @@ namespace MacroTrack.AppLibrary.ViewModels
     internal class GoalSetVM : ViewModelBase
     {
         public event Action<bool>? RequestClose;
+
+        public ObservableCollection<Goal> GoalList { get; } = new();
+
+        private Goal? _selectedGoal;
+        public Goal? SelectedGoal
+        {
+            get => _selectedGoal;
+            set
+            {
+                if (_selectedGoal == value) return;
+                _selectedGoal = value;
+                OnPropertyChanged();
+            }
+        }
 
         // Commands:
         public ICommand CloseCommand { get; }
@@ -29,6 +45,17 @@ namespace MacroTrack.AppLibrary.ViewModels
         public override void Init(CoreServices services, AppServices appServices)
         {
             base.Init(services, appServices);
+            Populate();
+        }
+
+        public void Populate()
+        {
+            Log();
+            if (Services == null) throw new Exception("Null Services");
+            List<Goal> goalList = Services.goalService.GetAllGoals();
+            GoalList.Clear();
+            foreach (Goal g in goalList) GoalList.Add(g);
+            
         }
 
         private void Delete()
