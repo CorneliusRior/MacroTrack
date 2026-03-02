798e197c764df5245d5dd23f609c921cdb1a4100
CorneliusRior <corneliuscriordan@gmail.com>
2026-03-02 00:35:15 +0000

    Gave help command HelpListType, list & full, nearly full rewrite on HelpCommand.

--- MacroTrack.Puppet2/Commands/HelpCommand.cs
+++ MacroTrack.Puppet2/Commands/HelpCommand.cs
@@ -1,6 +1,8 @@
-﻿using MacroTrack.Core.Services;
+﻿using DocumentFormat.OpenXml.Drawing.Charts;
+using MacroTrack.Core.Services;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -16,11 +18,24 @@ namespace MacroTrack.Puppet2.Commands
         public override IReadOnlyList<string> Aliases => new[] { "?", "h" };
         public override IReadOnlyList<CommandHelp> Help => 
         [
-            new(["Help"], "Help (command)", "Prints command information on all commands, or more detailed information on one if specified.", Aliases: Aliases)    
+            new(["Help"], 
+                "Help (command) and/or (string HelpListType)", 
+                "Prints command information on all commands, or more detailed information on one and its subcommands if specified.", 
+                Aliases: Aliases,
+                LongDescription: "HelpListTypes include: 'Description (default)', 'Usage', 'Example', 'Aliases': Only applies to short list. If no command is specified, will print the command and description, or specified HelpListType, for all commands which Puppet has access to."),
+            new(["Help", "List"],
+                "Help.List (command) (string HelpListType)",
+                "Prints command information on all commands in a single-line list form, or just commands specified and its subcommands.",
+                LongDescription: "HelpListTypes include: 'Description (default)', 'Usage', 'Example', 'Aliases'. If no command is specified, will print every command and description, or specified HelpListType, for all commands which Puppet has access to."),
+            new(["Help", "Full"],
+                "Help.Full (command)",
+                "Prints all available information for all commands, or for specified command and its subcommands if specified.")
+            
         ];
 
         public override PuppetResult Execute(IReadOnlyList<string> head, IReadOnlyList<string> args)
         {
+            /*
             var allHelp = _context.CommandList.SelectMany(c => c.Help).OrderBy(c => string.Join('.', c.Head));
             if (args.Count == 0)
             {
@@ -44,7 +59,112 @@ namespace MacroTrack.Puppet2.Commands
                 if (!string.IsNullOrWhiteSpace(c.Example)) longhelpstring += $"\n{"Example:", -15} {c.Example}";
                 if (!string.IsNullOrWhiteSpace(c.LongDescription)) longhelpstring += $"\n{c.LongDescription}";
             }
-            return PuppetResult.Ok(longhelpstring);
+            return PuppetResult.Ok(longhelpstring);*/
+
+            // Rewriting it here:
+            var allHelp = _context.CommandList.SelectMany(c => c.Help).OrderBy(c => string.Join('.', c.Head));
+
+            /* Possible command formats we want to be able to handle:
+             *  - help                      (head.Count == 0, args.count == 0)
+             *  - help food                 (head.Count == 0, args.count == 1)
+             *  - help food usage           (head.Count == 0, args.count == 2)
+             *  - help.list                 (head.Count == 1, args.count == 0)
+             *  - help.list food            (head.Count == 1, args.count == 1)
+             *  - help.list food usage      (head.Count == 1, args.count == 2)
+             *  - help.full                 (head.Count == 2, args.Count == 0)
+             *  - help.full food            (head.Count == 2, args.Count == 1)
+             * We'll iterate through args.Count.
+             */
+
+            if (head.Count < 2)
+            {
+                if (args.Count == 0) return ShortList(args, allHelp);
+                if (args.Count == 1)
+                {
+                    if (args[0].IsHelpListType()) return ShortList(args, allHelp);
+                    return LongList(args, allHelp);
+                }
+                else return ShortList(args, allHelp);
+                
+            }
+            return head[1].ToLowerInvariant().Trim() switch
+            {
+                "list"  => ShortList(args, allHelp),
+                "full"  => LongList(args, allHelp),
+                _ => PuppetResult.Fail($"Unknown subcommand '{Name}.{head[1]}'.")
+            };
+        }
+
+        private PuppetResult ShortList(IReadOnlyList<string> args, IOrderedEnumerable<CommandHelp> helpList)
+        {
+            /* 4 possible command formats:
+             *  - help
+             *  - help food
+             *  - help usage
+             *  - help food usage
+             */
+            HelpListType type = HelpListType.Description;
+
+            StringBuilder sb = new();
+            if (args.Count == 0)
+            {
+                sb.AppendLine("Printing all Commands:");
+            }
+            if (args.Count == 1)
+            {
+                // If we can parse first argument as a HelpListType, parse it as a command:
+                if (!args[0].IsHelpListType(out HelpListType t))
+                { // Can't parse:
+                    helpList = Search(args[0], helpList);
+                    sb.AppendLine($"Printing all Commands with head '{args[0].ToLowerInvariant().Trim()}':");
+                } // Can parse:
+                else
+                {
+                    type = t;
+                    sb.AppendLine($"Printing all Commands and {type.ToString(true)}:");
+                }
+            }
+            if (args.Count > 1)
+            {
+                // If there are 2 or more arguments, then the first one is command, the second one is HelpListType.
+                helpList = Search(args[0], helpList);
+                type = args[1].ToHelpListType();
+                sb.AppendLine($"Printing all Commands and {type.ToString(true)} with head '{args[0].ToLowerInvariant().Trim()}'");
+            }
+            //if (helpList.Count() == 0) return PuppetResult.Fail($"Unknown command '{args[0] ?? ""}'.");
+
+            sb.AppendLine(CommandHelp.PrintHeader(type));
+            foreach (CommandHelp c in helpList) sb.AppendLine(c.ShortList(type));
+            sb.AppendLine("Use 'help <command>' for more details.");
+            return PuppetResult.Ok(sb.ToString());
+        }
+
+        private PuppetResult LongList(IReadOnlyList<string> args, IOrderedEnumerable<CommandHelp> helpList)
+        {
+            p($"args={{ '{string.Join("', '", args)}' }}, helpList={{ '{string.Join("', '", helpList)}' }}");
+            StringBuilder sb = new();
+            if (args.Count > 0)
+            {
+                p($"Determined args.Count > 0. args[0]='{args[0]}'");
+                helpList = Search(args[0], helpList);
+                sb.AppendLine($"Printing all Commands with head '{args[0].ToLowerInvariant().Trim()}':");
+            }
+            else sb.AppendLine($"Printing all Commands:");
+            if (helpList.Count() == 0) return PuppetResult.Fail($"Unknown command '{args[0]}'.");
+            
+            foreach (CommandHelp c in helpList) sb.AppendLine(Environment.NewLine + c.LongList());            
+            return PuppetResult.Ok(sb.ToString());
+        }
+
+        private IOrderedEnumerable<CommandHelp> Search(string? searchString, IOrderedEnumerable<CommandHelp> allHelp)
+        {
+            if (string.IsNullOrWhiteSpace(searchString)) return allHelp;
+            p($"searchString='{searchString}', splitSearchString='{string.Join("', '", searchString.Split('.', StringSplitOptions.RemoveEmptyEntries))}'");
+            return allHelp.Where(
+                c => c.Head.HeadSearch(
+                    searchString.Split('.', StringSplitOptions.RemoveEmptyEntries)
+                    )
+                ).OrderBy(c => string.Join('.', c.Head));
         }
     }
 
@@ -61,7 +181,7 @@ namespace MacroTrack.Puppet2.Commands
             if (a.Count < b.Count) return false;
             for (int i = 0; i < a.Count && i < b.Count; i++)
             {
-                if (!a[i].Equals(b[i], StringComparison.OrdinalIgnoreCase)) return false;
+                if (!a[i].Equals(b[i], StringComparison.OrdinalIgnoreCase)) return false; 
             }
             return true;
         }
--- MacroTrack.Puppet2/Commands/PuppetCommandBase.cs
+++ MacroTrack.Puppet2/Commands/PuppetCommandBase.cs
@@ -1,4 +1,5 @@
-﻿using MacroTrack.Core.Services;
+﻿using MacroTrack.Core.Models;
+using MacroTrack.Core.Services;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -40,5 +41,91 @@ namespace MacroTrack.Puppet2.Commands
         }
     }
 
-    public sealed record CommandHelp(IReadOnlyList<string> Head, string Usage, string Description, string Example = "", string LongDescription = "", IReadOnlyList<string>? Aliases = null, IReadOnlyList<string>? SubCommands = null);
+    public sealed record CommandHelp(
+        IReadOnlyList<string> Head, 
+        string Usage, string Description,
+        string Example = "", 
+        string LongDescription = "", 
+        IReadOnlyList<string>? Aliases = null, 
+        IReadOnlyList<string>? SubCommands = null)
+    {
+        const int PCommandSpace = -25;
+        const int PHelpSpace = -175;
+        const string PIndent = " ";
+        /// <summary>
+        /// Only for use on ShortLists.
+        /// </summary>
+        public static string PrintHeader(HelpListType listType) =>
+            $"{"Command:", PCommandSpace}" +
+            $"{listType.ToString()}:";
+        public string ShortList(HelpListType listType) => 
+            $"{string.Join('.', Head).Truncate(PCommandSpace), PCommandSpace}" +
+            $"{listType switch {
+                HelpListType.Description => Description.Truncate(PHelpSpace),
+                HelpListType.Usage       => Usage.Truncate(PHelpSpace),
+                HelpListType.Example     => Example.Truncate(PHelpSpace),
+                HelpListType.Aliases     => Aliases is null ? "None" : $"{{ \"{string.Join($"\", \"", Aliases)}\" }}".Truncate(PHelpSpace),
+                _ => ""}, PHelpSpace}";
+        public string LongList() =>
+            $" * {string.Join('.', Head)}:\n" +
+            $"{(SubCommands is null ? "" : $"{PIndent}Subcommands: '{string.Join("', ", SubCommands)}'\n")}" +
+            $"{(Aliases is null ? "" : $"{PIndent}Aliases: {{ \"{string.Join($"\", \"", Aliases)}\" }}\n")}" +
+            $"{PIndent}Usage: {Usage}\n" +
+            $"{(string.IsNullOrWhiteSpace(Example) ? "" : $"{PIndent}Example: {Example}\n")}" +
+            $"{PIndent}{Description}" + 
+            $"{(string.IsNullOrWhiteSpace(LongDescription) ? "" : $"\n{PIndent}" + LongDescription)}";
+    }
+
+    public enum HelpListType
+    {
+        Description,
+        Usage,
+        Example,
+        Aliases
+    }
+
+    public static class HelpListTypeExtensions
+    {
+        public static HelpListType ToHelpListType(this string i) => i.ToLowerInvariant().Trim() switch
+        {
+            // Actual names:
+            "description" => HelpListType.Description,
+            "usage" => HelpListType.Usage,
+            "example" => HelpListType.Example,
+            "aliases" => HelpListType.Aliases,
+
+            // Numbers:
+            "0" => HelpListType.Description,
+            "1" => HelpListType.Usage,
+            "2" => HelpListType.Example,
+            "3" => HelpListType.Aliases,
+
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        public static bool IsHelpListType(this string i, out HelpListType helpListType)
+        {
+            // bool Name = string.IsHelpList(out var helpListType)
+            helpListType = HelpListType.Description; // default, you shouldn't use it.
+            try { helpListType = i.ToHelpListType(); }
+            catch { return false; }
+            return true;
+        }
+
+        public static bool IsHelpListType(this string i)
+        {
+            try { i.ToHelpListType(); }
+            catch { return false; }
+            return true;
+        }
+
+        public static string ToString(this HelpListType t, bool plural = false) => t switch
+        {
+            HelpListType.Description => plural ? "Descriptions" : "Description",
+            HelpListType.Usage       => plural ? "Usages" : "Usage",
+            HelpListType.Example     => plural ? "Examples" : "Example",   
+            HelpListType.Aliases     => "Aliases",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
 }
